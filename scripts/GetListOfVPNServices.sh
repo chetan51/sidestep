25c094560cb8a09e21b3aa912f46f729496c78c5
Chetan Surpur <chetan51@gmail.com>
2010-12-05 19:59:43 -0800

    Added script to get list of VPN services

---
+++ scripts/GetListOfVPNServices.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+# GetListOfVPNServices.sh
+# Sidestep
+#
+# Created by Chetan Surpur on 10/26/10.
+# Copyright 2010 Chetan Surpur. All rights reserved.
+
+# redirect stdin
+exec <"$0" || exit
+
+# find the start of the AppleScript
+found=0
+while read v; do
+        case "$v" in --*)
+                # file offset at start of AppleScript
+                found=1; break
+                ;;
+        esac
+done
+
+case "$found" in
+    0)  
+        echo 'AppleScript not found' >&2
+        exit 128
+        ;;
+esac
+
+# run the AppleScript
+bash -c "/usr/bin/osascript"; exit
+
+-- AppleScript starts here
+
+set vpn_services to {}
+
+tell application "System Events"
+	tell current location of network preferences
+		repeat with s in services
+			-- if kind of service = 14 then vpn
+			if kind of s is 14 then
+				copy name of s to end of my vpn_services
+			end if
+		end repeat
+	end tell
+end tell
+
+return vpn_services
