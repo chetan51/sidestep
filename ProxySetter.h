91595d9f83d8e81b200cf235a31e3cf5977b556b
Chetan Surpur <chetan51@gmail.com>
2010-12-06 13:40:56 -0800

    Moved AppUtilities to ProxySetter's .m file

--- ProxySetter.h
+++ ProxySetter.h
@@ -7,7 +7,6 @@
 //
 
 #import <Cocoa/Cocoa.h>
-#import "AppUtilities.h"
 
 @interface ProxySetter : NSObject {
 
--- ProxySetter.m
+++ ProxySetter.m
@@ -7,6 +7,7 @@
 //
 
 #import "ProxySetter.h"
+#import "AppUtilities.h"
 
 
 @implementation ProxySetter
